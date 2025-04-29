ba9160fef915f887c8d35e095c98736bf828bb95
www30206tw <asd70205@gmail.com>
2025-04-29 22:02:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -277,7 +277,6 @@ function initMapArea(){
   const tileData = tileMap.find(t => String(t.id) === hex.dataset.tileId);
   const rect = hex.getBoundingClientRect();
 
-  if {
     // —— 已放建築：三個懸浮框 —— 
 
     // 1. 完整卡牌
@@ -361,7 +360,6 @@ function initMapArea(){
    producePopup.style.top    = `${currentTop}px`;
    producePopup.style.left   = `${offsetX}px`;
    producePopup.style.display= 'block';
-     }
   });
 
     // 滑鼠移出：隱藏懸浮窗
